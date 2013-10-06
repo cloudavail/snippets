b54795b7513829ddb7e3f85fa7efb9b92b15893d
Colin Johnson <colinbjohnson@gmail.com>
2013-10-06 05:55:25 +0000

    Fabric API tutorial

---
+++ python/fabric/fabric_api/fabric_api_tutorial.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# objective: utilize the fabric api and environment variables to run a command
+# on remote hosts listed in env.hosts list
+
+import argparse
+import os
+
+from fabric.api import env
+from fabric.api import run
+from fabric.tasks import execute
+
+app_name = os.path.basename(__file__)
+
+# accept and parse user provided options
+parser = argparse.ArgumentParser()
+parser.add_argument('--task', help='command to run on remote server.', 
+    default='get_hostname', choices=['get_hostname','get_os'])
+args = parser.parse_args()
+task = args.task
+
+env.gateway = 'mygateway.mydomain.com'
+# by default each task will be run all of the hosts in the env.hosts list
+env.hosts = ['myhost01.mydomain.com','myhost02.mydomain.com']
+env.key_filename = '/path/to/.ssh/private_key'
+env.user = 'myusername'
+# env.use_ssh_config would serve as an alternative to setting env.gateway,
+# env.key_filename and env.user
+
+# task definition: get_hostname
+def get_hostname():
+    # run executable hostname on remote host(s)
+    print 'Calling run command: hostname'
+    run('hostname')
+
+# task definition: get_os
+def get_os():
+    # run executable uname on remote host(s)
+    print 'Calling run command: uname'
+    run('uname')
+
+if task == 'get_hostname':
+    execute(get_hostname)
+elif task == 'get_os':
+    execute(get_os)
