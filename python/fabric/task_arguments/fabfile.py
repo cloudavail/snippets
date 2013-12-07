dfabff146348cba3986c9da5654ee87c60f839ba
Colin Johnson <colinbjohnson@gmail.com>
2013-12-07 07:21:46 +0000

    initial commit of fabric task argument snippets.

--- python/fabric/task_arguments/fabfile.py
+++ python/fabric/task_arguments/fabfile.py
@@ -1,5 +1,6 @@
 # objective: to understand per task arguments given at command line
 
+
 # per task argument
 # to test the task argument, run any of the following:
 # fab single_task_argument
@@ -7,7 +8,8 @@
 # or
 # fab single_task_argument:orange
 def single_task_argument(fruit='apple'):
-    print('Fruit is: {fruit!s}'.format (fruit=fruit))
+    print 'Fruit is: {!s}'.format(fruit)
+
 
 # multiple per task arguments
 # to test multiple task argument, run any of the following:
@@ -16,5 +18,5 @@ def single_task_argument(fruit='apple'):
 # or
 # fab multiple_task_argument:orange,eggplant
 def multiple_task_argument(fruit='apple', vegetable='artichoke'):
-    print('Fruit is: {fruit!s}'.format (fruit=fruit))
-    print('Vegetable: {vegetable!s}'.format (vegetable=vegetable))
+    print 'Fruit is: {!s}'.format(fruit)
+    print 'Vegetable: {!s}'.format(vegetable)
