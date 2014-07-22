a59a26408a9fb91dffaf45d0cafab135ecc0706a
Colin Johnson <colinbjohnson@gmail.com>
2014-07-22 17:59:36 +0000

    spell watermelon correctly and pep-8 fixup

--- python/functions/functions_accept_keyword_arguments/functions_accept_keyword_arguments.py
+++ python/functions/functions_accept_keyword_arguments/functions_accept_keyword_arguments.py
@@ -2,13 +2,14 @@
 
 # if a keyword argument does not exist it must have a default argument
 #
-# keyword_alpha has no default value - failure to provide this keyword when 
+# keyword_alpha has no default value - failure to provide this keyword when
 # calling my_function would result in a failure
 #
 # keyword_beta has a default value of 'watermelon' but this will be overwritten
 # by the passed in value 'pineapple'
-#
-def my_function(positional_arg, keyword_alpha, keyword_beta='watermellon'):
+
+
+def my_function(positional_arg, keyword_alpha, keyword_beta='watermelon'):
     print positional_arg
     print keyword_alpha
     print keyword_beta
