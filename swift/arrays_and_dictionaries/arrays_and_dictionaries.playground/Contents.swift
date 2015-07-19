1a59a0971fc96463fc4721582dbb009ca5d754de
Colin Johnson <colinbjohnson@gmail.com>
2015-07-19 19:44:04 +0000

    initial commit of arrays_and_dictionaries

---
+++ swift/arrays_and_dictionaries/arrays_and_dictionaries.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+var mlbTeamNamesArray = [ "Giants", "Athletics" ]
+var mlbCityToTeamDictionary = [ "san_francisco": "Giants", "oakland": "Athletics" ]
+
+mlbTeamNamesArray[0]
+mlbCityToTeamDictionary["san_francisco"]
+
+// create an Empty Array that will contain String type Objects
+var emptyArray = [String]()
+// create an Empty Dictionary that will contain Keys of type String and Values of type Int
+var emptyDictionary = [String: Int]()
