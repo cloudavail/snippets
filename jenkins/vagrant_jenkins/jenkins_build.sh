54964995fc1c5af4fdfbc38aa8cca18ec602f12b
Colin Johnson <colinbjohnson@gmail.com>
2016-02-21 19:52:09 +0000

    move to trusty and Jenkins repository

--- jenkins/vagrant_jenkins/jenkins_build.sh
+++ jenkins/vagrant_jenkins/jenkins_build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# installs of all jenkins dependencies
+sudo apt-get update
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
 sudo apt-get -y install jenkins
+service jenkins restart
 
-# Restart jenkins service
-sudo service jenkins restart
+# access Jenkins: http://192.168.2.2:8080/
 
 # update Jenkins to latest version
-sudo service jenkins stop
-cd /usr/share/jenkins
-rm jenkins.war
-wget http://updates.jenkins-ci.org/latest/jenkins.war
-sudo service jenkins start
-sudo service jenkins restart
+# service jenkins stop
+# cd /usr/share/jenkins
+# rm jenkins.war
+# wget http://updates.jenkins-ci.org/latest/jenkins.war
+# sudo service jenkins start
+# sudo service jenkins restart
