da9350c69cff5550824a075933358625f8a3e14e
Colin Johnson <colinbjohnson@gmail.com>
2016-04-03 21:46:57 +0000

    resolution of issues with writing to s3 - now able to do so

--- sqoop/basic_sqoop/basic_sqoop.sh
+++ sqoop/basic_sqoop/basic_sqoop.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 apt-get -y install openjdk-8-jdk
+# resolve issue with certificates when using aws-java-sdk classes
+apt-get -y install ca-cacert
+
 
 cd /usr/local/bin
+
+# note: file download is 203 MB
+wget --quiet http://mirrors.ibiblio.org/apache/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz
+tar xf hadoop-2.7.2.tar.gz
+ln -s /usr/local/bin/hadoop-2.7.2 /usr/local/bin/hadoop.dir
 
 # note: the value __hadoop-x.y.z reflects the /lowest/ version of hadoop that must be installed
 # __hadoop-1.0.0 means any version /newer/ than version 1 - although I do not know if
@@ -11,16 +19,21 @@ cd /usr/local/bin
 wget --quiet http://www.trieuvan.com/apache/sqoop/1.4.6/sqoop-1.4.6.bin__hadoop-2.0.4-alpha.tar.gz
 tar xf sqoop-1.4.6.bin__hadoop-2.0.4-alpha.tar.gz
 ln -s /usr/local/bin/sqoop-1.4.6.bin__hadoop-2.0.4-alpha/bin/sqoop /usr/local/bin/sqoop
+cd /var/tmp/
+wget --quiet https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.38.tar.gz
+tar xf mysql-connector-java-5.1.38.tar.gz
+cp /var/tmp/mysql-connector-java-5.1.38/mysql-connector-java-5.1.38-bin.jar /usr/local/bin/sqoop-1.4.6.bin__hadoop-2.0.4-alpha/lib/
 
-# note: file download is 203 MB
-wget --quiet http://mirrors.ibiblio.org/apache/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz
-tar xf hadoop-2.7.2.tar.gz
-ln -s /usr/local/bin/hadoop-2.7.2 /usr/local/bin/hadoop
+# Copy required libraries into existing Classpath
+# I could find no method of modifying classpath when running sqoop
+# modifying $HADOOP_CLASSPATH an option - but making this change not viable because I'm not sure what changing it does
+cp /usr/local/bin/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-aws-2.7.2.jar /usr/local/bin/sqoop-1.4.6.bin__hadoop-2.0.4-alpha/lib/
+cp /usr/local/bin/hadoop-2.7.2/share/hadoop/tools/lib/aws-java-sdk-1.7.4.jar /usr/local/bin/sqoop-1.4.6.bin__hadoop-2.0.4-alpha/lib/
 
 # set environment variables required to run sqoop
 cat > /etc/profile.d/hadoop_env_variables.sh <<EOF
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-export HADOOP_HOME=/usr/local/bin/hadoop
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/usr/local/bin/hadoop.dir
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
 export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
@@ -36,4 +49,3 @@ source /etc/profile.d/hadoop_env_variables.sh
 # sqoop export --connect jdbc:mysql://myrds.crezaaaruhfx.us-west-2.rds.amazonaws.com/pipetest \
 # --username mannem --password Password123 --table dailyStockPrices --input-fields-terminated-by '\t' \
 # --input-lines-terminated-by '\n' --export-dir s3://mannem/sqoopmatrix/part-m-00000
-
