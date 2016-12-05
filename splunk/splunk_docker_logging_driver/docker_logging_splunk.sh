e765b7d3d0b04985788e426803959b007bcb8fc6
Colin Johnson <colinbjohnson@gmail.com>
2016-12-05 20:02:40 +0000

    update docker_logging_splunk - completed send of logs

--- splunk/splunk_docker_logging_driver/docker_logging_splunk.sh
+++ splunk/splunk_docker_logging_driver/docker_logging_splunk.sh
@@ -3,23 +3,27 @@
 # objective:
 # to output logs from Docker to splunk
 splunk_token=""
+# note that the Splunk URL may be different depending on the type of Splunk installation you are using.
+# http://docs.splunk.com/Documentation/SplunkCloud/6.5.1/User/AdddatausingHTTPeventcollector
+# in particular:
+# for Splunk Light Cloud Instance: https://prd-p-32qdqp6x3ktt.cloud.splunk.com
+# the HTTP Event Collector URL will prepend "input-" to the host part of the FQDN
+# https://input-prd-p-32qdqp6x3ktt.cloud.splunk.com:8088/services/collector/event
 splunk_url=""
 
 # install Docker
 apt -y install docker.io
 
 docker pull nginx:latest
-# the command below will:
-# utilize the splunk log driver
-# configure the splunk log driver to send data to a splunk instance
-# run a docker container running nginx
-# publish that port to the "host" system
-# more info:
+# the command below is described by:
+# --log-driver=splunk = utilize the splunk log driver
+# --log-opt splunk-token=$splunk_token = use the splunk token defined for the given HTTP Event Collector
+# --log-opt splunk-url=$splunk_url = send events to the given URL
+# NOTE: the below is strongly discouraged from Production systems
+# --log-opt splunk-insecureskipverify=True = will disregard a fatal error if the certificate is self-signed
 # https://docs.docker.com/engine/admin/logging/splunk/
-
-# test availability of port?
-# curl --insecure $splunk_url
 
 docker run --log-driver=splunk --log-opt splunk-token=$splunk_token \
            --log-opt splunk-url=$splunk_url \
+           --log-opt splunk-insecureskipverify=True \
            --name docker-nginx --detach --publish 8080:80 nginx
