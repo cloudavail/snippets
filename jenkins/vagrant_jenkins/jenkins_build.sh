5e7e04ca1178369ab82748aece19ed9dee77e864
Colin Johnson <colinbjohnson@gmail.com>
2016-12-22 22:25:54 +0000

    move jenkins in vagrant to xenial

--- jenkins/vagrant_jenkins/jenkins_build.sh
+++ jenkins/vagrant_jenkins/jenkins_build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-sudo apt-get update
+# from
+# https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+
+apt -y update
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
-sudo apt-get update
-sudo apt-get -y install jenkins
-service jenkins restart
+apt -y update
+apt -y install jenkins
+systemctl restart jenkins
 
 # access Jenkins: http://192.168.2.2:8080/
 
 # update Jenkins to latest version
-# service jenkins stop
+# systemctl stop jenkins
 # cd /usr/share/jenkins
 # rm jenkins.war
 # wget http://updates.jenkins-ci.org/latest/jenkins.war
-# sudo service jenkins start
-# sudo service jenkins restart
+# systemctl stop start
