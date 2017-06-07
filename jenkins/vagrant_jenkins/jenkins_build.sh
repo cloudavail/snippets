76faa0ddae388ffd00a1e1235425c68b4958b311
Colin Johnson <colinbjohnson@gmail.com>
2017-06-07 20:33:18 +0000

    update Jenkins install with latest instructions

--- jenkins/vagrant_jenkins/jenkins_build.sh
+++ jenkins/vagrant_jenkins/jenkins_build.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 # from
-# https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+# https://jenkins.io/download/
 
-apt -y update
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb https://pkg.jenkins.io/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
 apt -y update
 apt -y install jenkins
 systemctl restart jenkins
 
 # access Jenkins: http://192.168.2.2:8080/
-
-# update Jenkins to latest version
-# systemctl stop jenkins
-# cd /usr/share/jenkins
-# rm jenkins.war
-# wget http://updates.jenkins-ci.org/latest/jenkins.war
-# systemctl stop start
