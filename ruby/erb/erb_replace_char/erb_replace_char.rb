9eea0170e9582fac5bc93089a98bd13c1240acb6
Colin Johnson <colinbjohnson@gmail.com>
2018-07-31 15:47:20 -0700

    initial commit of erb_replace_char

--- ruby/erb/erb_replace_char/erb_replace_char.rb
+++ ruby/erb/erb_replace_char/erb_replace_char.rb
@@ -2,9 +2,6 @@
 
 require 'erb'
 
-string_with_underscore = "this_is_a_string_with_underscore_characters"
-string_with_underscore
-
 string_with_underscore = "this_is_a_string_where_underscore_characters_will_be_replaced"
 string_after_erb_substitution = ERB.new("<%= string_with_underscore.gsub!('_', '-') %>")
 puts string_after_erb_substitution.result(binding)
