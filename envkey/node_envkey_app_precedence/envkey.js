1a238313beb328c63c5a0ebae0def6c9e4b0f354
Colin Johnson <colin@cloudavail.com>
2020-02-17 10:22:11 -0800

    initial commit of node_envkey_app_precedence

---
+++ envkey/node_envkey_app_precedence/envkey.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// understanding of precendece
+// environment variable
+// .env variable
+// envkey variable
+
+require('envkey')
+// envkey, if loaded properly, will modify "env" to include values from envkey
+console.log(process.env)
