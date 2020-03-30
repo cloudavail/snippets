e482236a85ee20299677b2c60b1a49f214ccd2c0
Colin Johnson <colin@cloudavail.com>
2020-03-30 12:01:12 -0700

    initial value of hash value or default

--- ruby/hash_value_or_default/hash_value_or_default.rb
+++ ruby/hash_value_or_default/hash_value_or_default.rb
@@ -6,3 +6,4 @@ record_not_exist = {}
 puts record_exist[:stream] # stdout
 puts record_not_exist[:stream] # 
 puts record_not_exist[:stream] || "Not Defined"
+puts record_not_exist.fetch(:stream, "Not Defined")
