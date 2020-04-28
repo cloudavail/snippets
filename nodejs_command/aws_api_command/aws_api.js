294d48c9dfceb3056b7cb10dc12e1d1526cde152
Colin Johnson <colin@cloudavail.com>
2020-04-28 12:16:28 -0700

    move double to single quotes

--- nodejs_command/aws_api_command/aws_api.js
+++ nodejs_command/aws_api_command/aws_api.js
@@ -7,8 +7,8 @@ aws.config.region = 'us-west-2'
 var s3 = new aws.S3();
 s3.listBuckets(function(err, data) {
   if (err) {
-    console.log("Error", err);
+    console.log('Error', err);
   } else {
-    console.log("Success", data.Buckets);
+    console.log('Success', data.Buckets);
   }
 });
