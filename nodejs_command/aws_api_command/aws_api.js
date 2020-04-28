071a02ad0651288847122592760db42d681890f6
Colin Johnson <colin@cloudavail.com>
2020-04-28 12:15:15 -0700

    update package name and remove cfn call, move to two spaces

--- nodejs_command/aws_api_command/aws_api.js
+++ nodejs_command/aws_api_command/aws_api.js
@@ -6,18 +6,9 @@ aws.config.region = 'us-west-2'
 //call S3 to list the buckets
 var s3 = new aws.S3();
 s3.listBuckets(function(err, data) {
-	if (err) {
-		console.log("Error", err);
-	} else {
-		console.log("Success", data.Buckets);
-	}
-});
-
-//call cloudformation to describe stack
-var cfn = new aws.CloudFormation();
-cfn.describeStacks({StackName: "mediaconvert"}, function(err, data) {
-    // output the entire "Stack" response
-    console.log(data);
-    // output only the Stack's Output
-    console.log(data.Stacks[0].Outputs);
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Buckets);
+  }
 });
