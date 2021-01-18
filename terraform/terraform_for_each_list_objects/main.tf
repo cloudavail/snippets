3e32b0fed0cb962e8c9d5e843cb7fdb488169b3c
Colin Johnson <colinjohnson@cjohnson07.local>
2021-01-17 16:42:10 -0800

    foreach with list of objects

--- terraform/terraform_for_each_list_objects/main.tf
+++ terraform/terraform_for_each_list_objects/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 resource "local_file" "local_file_generator" {
-  for_each = { for cidr_range_objects in var.cidr_ranges : cidr_range_objects.filename => cidr_range_objects }
-  filename = "${path.module}/${(lookup(each.value, "filename"))}.txt"
-  content  = (lookup(each.value, "cidr_block"))
+  for_each = { for cidr_range in var.cidr_ranges : cidr_range["cidr_block"] => cidr_range }
+  filename = each.value.filename
+  content  = each.value.cidr_block
 }
--- terraform/terraform_for_each_list_objects/terraform.tfvars
+++ terraform/terraform_for_each_list_objects/terraform.tfvars
@@ -1,4 +1,4 @@
 cidr_ranges = [
   { "cidr_block" : "192.168.0.0/24", "filename" : "first_cidr_range.txt" },
-  { "cidr_block" : "192.168.0.0/24", "filename" : "second_cidr_rangetxt" }
+  { "cidr_block" : "192.168.0.128/24", "filename" : "second_cidr_rangetxt" }
 ]
