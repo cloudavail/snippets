db0c96564b213b7c71daa2b28c95ca8bd5e2c292
mchan9125 <mchan9125@gmail.com>
2021-06-18 12:47:42 -0700

    initial commit

---
+++ playwright/playwright_with_headless_true/playwright_with_headless_true.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://www.cloudavail.com/")
+    print(page.title())
+    browser.close()
