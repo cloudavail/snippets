76b12f30309eb5c99701a72c336cdbd632c3548b
mchan9125 <mchan9125@gmail.com>
2021-12-27 10:14:03 -0800

    initial commit of click vacation.py

--- python/click/basic_click_project/vacation.py
+++ python/click/basic_click_project/vacation.py
@@ -1,32 +1,35 @@
-import argparse
+import click
 
+@click.group('Location')
+def location():
+    """Where to go on vacation?"""
+# top
+@location.group()
+def spain():
+    """Choose a city"""
 
-def main():
-    parser = argparse.ArgumentParser(description="Decide where to go on vacation!")
-    parser.add_argument("-l", "--location", help="choose a valid location to go on vacation",
-                        choices=["spain", "hawaii", "Spain", "Hawaii"])
-    parser.add_argument("-t", "--todo", help="example usage: --location Hawaii --todo beach,"
-                                             " no functionality beyond when used with Hawaii", choices=["beach", "hiking"])
-    parser.add_argument("-c", "--city", help="example usage: --location Spain --city Madrid,"
-                                             " no functionality beyond when used with Spain", choices=["Madrid", "Barcelona"])
-    args = parser.parse_args()
-    if args.location in ["Hawaii", "hawaii", "HAWAII"]:
-        if args.todo in ["beach", "Beach", "BEACH"]:
-            print("Have fun at the beach!")
-        elif args.todo in ["Hiking", "hiking", "HIKING"]:
-            print("Have fun hiking!")
-        else:
-            print("Please choose a valid activity in Hawaii")
-    elif args.location in ["Spain", "spain", "SPAIN"]:
-        if args.city in ["Madrid", "madrid, MADRID"]:
-            print("Enjoy Madrid!")
-        elif args.city in ["Barcelona", "barcelona", "BARCELONA"]:
-            print("Enjoy Barcelona!")
-        else:
-            print("Please choose a valid city in Spain")
-    else:
-        print("Please refer to --help or options menu for proper use")
+@location.group()
+def hawaii():
+    """Choose an activity"""
 
+# level 2
+# spain
+@spain.command()
+def madrid():
+    click.echo(f'Enjoy Madrid!')
+
+@spain.command()
+def barcelona():
+    click.echo(f'Enjoy Barca!')
+
+# hawaii
+@hawaii.command()
+def beach():
+    click.echo(f'Lets go to the beach!')
+
+@hawaii.command()
+def hiking():
+    click.echo(f'Hiking sounds fun!')
 
 if __name__ == "__main__":
-    main()
+    location()
