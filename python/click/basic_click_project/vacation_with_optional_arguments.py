b7f2517ffea258e81ba83fd6879b7bed27ebe280
mchan9125 <mchan9125@gmail.com>
2021-12-27 13:38:07 -0800

    initial commit of click vacation_with_optional_arguments.py

--- python/click/basic_click_project/vacation_with_optional_arguments.py
+++ python/click/basic_click_project/vacation_with_optional_arguments.py
@@ -1,16 +1,21 @@
 import click
 
+
 @click.group('Location')
 def location():
     """Where to go on vacation?"""
+
+
 # top
 @location.group()
 def spain():
     """Choose a city"""
 
+
 @location.group()
 def hawaii():
     """Choose an activity"""
+
 
 # level 2
 # spain
@@ -18,18 +23,23 @@ def hawaii():
 def madrid():
     click.echo(f'Enjoy Madrid!')
 
+
 @spain.command()
 def barcelona():
     click.echo(f'Enjoy Barca!')
+
 
 # hawaii
 @hawaii.command()
 def beach():
     click.echo(f'Lets go to the beach!')
 
+
 @hawaii.command()
-def hiking():
-    click.echo(f'Hiking sounds fun!')
-    
+@click.option('--trail-length', type=int, default=2, help='Length of trail in miles')
+def hiking(trail_length):
+    click.echo(f'Hiking sounds fun! It will be {trail_length} miles long')
+
+
 if __name__ == "__main__":
     location()
